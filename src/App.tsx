f5a468884e967a1967a9874857dd4e37d890db3c
Emmanuel <ayemma2003@gmail.com>
2026-07-20 20:16:16 +0100

    Fix component filename casing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
   Route,
 } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navber";
 import Footer from "./components/Footer";
 import AdminRoute from "./components/AdminRoute";
 
